84e3c3902d8bd5e13c75253d25b721b5d51d90c7
sabanonitsuke <sabakan.nari@gmail.com>
2023-04-30 00:54:06 +0900

    最低限の機能実装

--- application.rb
+++ application.rb
@@ -1,6 +1,39 @@
 require "./statement"
 
-assets = Statement.new("資産", ["現金", "売掛金"])
-assets.show
-assets.check("現金")
+assets = Statement.new("資産", ["現金", "預金", "土地", "備品", "車両運搬具", "未収入金"])
+liabilities = Statement.new("負債", ["買掛金", "未払金"])
+equities = Statement.new("純資産", ["資本金", "繰越利益剰余金", "利益準備金"])
+expenses = Statement.new("費用", ["支払利息", "仕入"])
+revenues = Statement.new("収益", ["受取利息", "売上", "固定資産売買益"])
 
+puts "行いたいことを選択してください"
+puts "【0】勘定科目から調べる"
+puts "【1】5大科目から一覧を見る"
+selected = gets.chomp.to_i
+
+if selected == 0
+  puts "勘定科目を入力してください"
+  selected = gets.chomp
+  assets.check(selected)
+  liabilities.check(selected)
+  equities.check(selected)
+  expenses.check(selected)
+  revenues.check(selected)
+elsif selected == 1
+  puts "一覧を見たい5大科目を入力してください"
+  selected = gets.chomp
+  case selected
+  when "資産"
+    assets.show
+  when "負債"
+    liabilities.show
+  when "純資産"
+    equities.show
+  when "費用"
+    expenses.show
+  when "収益"
+    revenues.show
+  else
+    puts "見つかりませんでした"
+  end
+end
